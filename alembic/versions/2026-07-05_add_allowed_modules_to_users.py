0e42cebe42b63bed364fc01887e7e5492443c237
TeamTheInquisitive <team.theinquisitive@gmail.com>
2026-07-05 20:54:58 +0530

    fix

--- alembic/versions/2026-07-05_add_allowed_modules_to_users.py
+++ alembic/versions/2026-07-05_add_allowed_modules_to_users.py
@@ -1,6 +1,6 @@
 """Add allowed_modules column to users table
 
-Revision ID: a1b2c3d4e5f6
+Revision ID: f7a8b9c0d1e2
 Revises: f6a7b8c9d0e1
 Create Date: 2026-07-05 10:00:00.000000
 
@@ -14,7 +14,7 @@ from alembic import op
 
 
 # revision identifiers, used by Alembic.
-revision: str = 'a1b2c3d4e5f6'
+revision: str = 'f7a8b9c0d1e2'
 down_revision: Union[str, None] = 'f6a7b8c9d0e1'
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
